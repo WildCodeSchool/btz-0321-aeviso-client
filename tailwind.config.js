05b3e14a20613fdb90abf122299c910620d8a25c
Basile Vernouillet <basile64.v@gmail.com>
2021-05-26 14:48:09 +0200

    init :fire:

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import("@types/tailwindcss/tailwind-config").TailwindConfig } */
+module.exports = {
+  purge: ["./src/**/*.{js,jsx,ts,tsx}"],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+};
