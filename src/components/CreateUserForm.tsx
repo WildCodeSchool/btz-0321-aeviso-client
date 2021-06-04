e1e742937b1926252f5fa8d3f3f1013fcdb3b6c0
MatthiasWanner <matthias.wanner@gmail.com>
2021-06-04 14:08:59 +0200

    Create user form done

--- src/components/CreateUserForm.tsx
+++ src/components/CreateUserForm.tsx
@@ -1,39 +1,94 @@
 import React, { useState } from "react";
-import { useQuery } from "react-query";
-import { useParams } from "react-router-dom";
+import { useMutation, useQuery } from "react-query";
 
-interface Data {
+interface User {
   firstname: string;
   lastname: string;
   email: string;
   profession: string | null;
 }
 
-function User() {
+function CreateUserForm() {
   const [firstname, setFirstname] = useState<string>("");
-  const [lastname, setLastname] = useState<string>("");
+  const [lastname, setLastName] = useState<string>("");
   const [email, setEmail] = useState<string>("");
   const [profession, setProfession] = useState<string>("");
-  const { isLoading, error, data } = useQuery<Data, Error>("user", () => {
-    fetch(`http://localhost:5000/api/v1/users`, {
-      method: "POST",
-      body: JSON.stringify({
-        firstname: firstname,
-        lastname: lastname,
-        email: email,
-        profession: profession,
-      }),
-    });
-  });
 
-  if (isLoading) return "Loading...";
-  if (error) return "An error has occurred: " + error.message;
+  const mutation = useMutation((user: User) =>
+    fetch("http://localhost:5000/api/v1/users", {
+      method: "POST",
+      headers: {
+        "Content-type": "application/json; charset=UTF-8",
+      },
+      body: JSON.stringify(user),
+    })
+      .then((response) => response.json())
+      .then((json) => console.log(json))
+  );
+
+  const handleSubmit = (e: Event) => {
+    const newUser = {
+      firstname,
+      lastname,
+      email,
+      profession,
+    };
+    mutation.mutate(newUser);
+  };
+
+  if (mutation.isLoading) return <p>Sending User...</p>;
+  if (mutation.error) return <p>An error has occurred</p>;
+  if (mutation.isSuccess) return <p>User successfuly added</p>;
+
   return (
     <div>
       <h3 className="mb-6">Create User</h3>
-      <div className="border border-black mb-2"></div>
+      <div className="border border-black mb-2">
+        <form onSubmit={handleSubmit}>
+          <div className="form-example">
+            <label htmlFor="firstName">Enter your first name: </label>
+            <input
+              type="text"
+              name="firstName"
+              id="firstName"
+              value={firstname}
+              onChange={(e) => setFirstname(e.target.value)}
+            />
+          </div>
+          <div className="form-example">
+            <label htmlFor="lastName">Enter your last name: </label>
+            <input
+              type="text"
+              name="lastName"
+              id="lastName"
+              onChange={(e) => setLastName(e.target.value)}
+            />
+          </div>
+          <div className="form-example">
+            <label htmlFor="email">Enter your email: </label>
+            <input
+              type="email"
+              name="email"
+              id="email"
+              onChange={(e) => setEmail(e.target.value)}
+            />
+          </div>
+          <div className="form-example">
+            <label htmlFor="job">Enter your job: </label>
+            <input
+              type="job"
+              name="job"
+              id="job"
+              onChange={(e) => setProfession(e.target.value)}
+            />
+          </div>
+          <div className="form-example">
+            <input type="submit" value="Create new user" />
+          </div>
+        </form>
+      </div>
     </div>
   );
 }
 
-export default User;
+export default CreateUserForm;
