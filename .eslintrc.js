5e232e4924e01c18d10f97a02df98640548e2436
Basile Vernouillet <basile64.v@gmail.com>
2021-06-16 11:56:32 +0200

    add eslint workflow

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,15 +1,17 @@
 module.exports = {
   root: true, // Make sure eslint picks up the config at the root of the directory
+  parser: '@typescript-eslint/parser',
   parserOptions: {
     ecmaVersion: 2020, // Use the latest ecmascript standard
-    sourceType: "module", // Allows using import/export statements
+    sourceType: 'module', // Allows using import/export statements
     ecmaFeatures: {
       jsx: true, // Enable JSX since we're using React
     },
   },
+  plugins: ['@typescript-eslint'],
   settings: {
     react: {
-      version: "detect", // Automatically detect the react version
+      version: 'detect', // Automatically detect the react version
     },
   },
   env: {
@@ -18,19 +20,20 @@ module.exports = {
     node: true, // Enables Node.js global variables and Node.js scoping.
   },
   extends: [
-    "eslint:recommended",
-    "plugin:react/recommended",
-    "plugin:jsx-a11y/recommended",
-    "plugin:prettier/recommended", // Make this the last element so prettier config overrides other formatting rules
+    'eslint:recommended',
+    'plugin:@typescript-eslint/eslint-recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:react/recommended',
+    'plugin:jsx-a11y/recommended',
+    'plugin:prettier/recommended', // Make this the last element so prettier config overrides other formatting rules
   ],
   rules: {
-    "prettier/prettier": ["error", {}, { usePrettierrc: true }], // Use our .prettierrc file as source
-    "react/function-component-definition": [
-      2,
-      { namedComponents: "function-declaration" },
-    ],
-    "no-console": 1,
-    "react/prop-types": 1,
-    "react/jsx-filename-extension": "error",
+    'prettier/prettier': ['error', {}, { usePrettierrc: true }], // Use our .prettierrc file as source
+    'react/function-component-definition': [2, { namedComponents: 'function-declaration' }],
+    'no-console': 1,
+    'react/prop-types': 1,
+    'no-unused-vars': 'off',
+    '@typescript-eslint/no-unused-vars': ['error', { argsIgnorePattern: '^_', ignoreRestSiblings: true }],
+    'react/jsx-filename-extension': [2, { extensions: ['.tsx', '.jsx'] }],
   },
 };
