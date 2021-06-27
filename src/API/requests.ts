2897293c8791bce1e625dac225230c8a054b44bc
MatthiasWanner <matthias.wanner@gmail.com>
2021-06-27 07:41:56 +0200

    Request identity in layout

--- src/API/requests.ts
+++ src/API/requests.ts
@@ -92,4 +92,6 @@ export const records = {
 export const auth = {
   login: (user: { email: string; password: string }): Promise<{ message: string; user: User }> =>
     axios.post(`${API_URL}/auth/login`, user).then((res) => res.data),
+
+  me: (): Promise<{ message: string; user: User }> => axios.get(`${API_URL}/auth/me`).then((res) => res.data),
 };
