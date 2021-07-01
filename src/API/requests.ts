f1eebc5176843071c6e5e3fffe025e0ce585a860
MatthiasWanner <matthias.wanner@gmail.com>
2021-07-01 10:12:42 +0200

    Add request in layout that verify if user is auth

--- src/API/requests.ts
+++ src/API/requests.ts
@@ -1,6 +1,7 @@
 import axios from 'axios';
+const API_URL = import.meta.env.VITE_API_URL || 'http://localhost:5000';
 
-const API_URL = import.meta.env.VITE_API_URL || 'http://localhost:5000/api/v1';
+axios.defaults.withCredentials = true;
 
 export const user = {
   getAll: (): Promise<User[]> => axios.get(`${API_URL}/users`).then((res) => res.data),
--- src/views/Layout.tsx
+++ src/views/Layout.tsx
@@ -20,6 +20,7 @@ function Layout(): JSX.Element {
 
   const { isLoading } = useQuery<{ message: string; user: User }>('userAuthenticated', () => auth.me(), {
     onSuccess: (data) => {
+      console.log(data);
       const { user } = data;
       store.dispatch({
         type: actions.LOGIN,
