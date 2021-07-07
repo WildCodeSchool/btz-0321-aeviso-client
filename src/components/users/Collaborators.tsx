0288b3da9b958c3999a624a77523ddd053350980
MatthiasWanner <matthias.wanner@gmail.com>
2021-07-07 20:43:31 +0200

    Add header component, react hookform that will filter result

--- src/components/users/Collaborators.tsx
+++ src/components/users/Collaborators.tsx
@@ -1,14 +1,16 @@
 import { AxiosError } from 'axios';
 import React from 'react';
+import { useForm } from 'react-hook-form';
 import { useQuery } from 'react-query';
 
 import { companies } from '../../API/requests';
 import { useUserFromStore } from '../../store/user.slice';
+import MainComponentHeader from '../MainComponentHeader';
 import Spinner from '../Spinner';
 import UserPreview from './UserPreview';
 
 function Collaborators(): JSX.Element {
-  //   const history = useHistory();
+  const { register } = useForm();
   const { user } = useUserFromStore();
 
   const { isLoading, error, data } = useQuery<User[], AxiosError>('users', () =>
@@ -25,9 +27,12 @@ function Collaborators(): JSX.Element {
 
   return (
     <div className="dark:bg-component bg-white border-2 dark:border-componentBorder h-full sm:w-full text-black dark:text-white font-roboto rounded-xl shadow-mainShadow mx-4 sm:mx-0  sm:px-10 p-5">
-      {data?.map((user) => {
-        return <UserPreview key={user.id} id={user.id as string} />;
-      })}
+      <MainComponentHeader register={register} title="Liste des collaborateurs" />
+      <div className="sm:mt-10 mt-5">
+        {data?.map((user) => {
+          return <UserPreview key={user.id} id={user.id as string} />;
+        })}
+      </div>
     </div>
   );
 }
