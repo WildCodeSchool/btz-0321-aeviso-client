cdc90c4d9d7e446e5bda73f3835e30a94a1269ee
MatthiasWanner <matthias.wanner@gmail.com>
2021-07-07 14:46:59 +0200

    Move and rename UsersJob created by Chabelle to use it else where

---
+++ src/components/jobs/JobDisplay.tsx
@@ -0,0 +1,26 @@
+import React from 'react';
+import { useQuery } from 'react-query';
+import { AxiosError } from 'axios';
+
+import { jobs } from '../../API/requests';
+import Spinner from '../Spinner';
+
+function JobDisplay({ id }: { id: string }): JSX.Element {
+  const { isLoading, error, data } = useQuery<Job, AxiosError>(['jobs', id], () => jobs.getOne(id));
+
+  if (isLoading) {
+    return <Spinner />;
+  }
+
+  if (error) {
+    return (
+      <p>
+        An error has occurred: {error.message}. Code: {error.code}
+      </p>
+    );
+  }
+
+  return <>{data?.label}</>;
+}
+
+export default JobDisplay;
