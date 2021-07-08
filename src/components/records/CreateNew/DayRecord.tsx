2d184377853f7084c6b50febef8575bf008d79ee
Thomas <barrialthomas@gmail.com>
2021-07-08 11:27:55 +0200

    Add modal / put / delete roads

--- src/components/records/CreateNew/DayRecord.tsx
+++ src/components/records/CreateNew/DayRecord.tsx
@@ -5,6 +5,10 @@ import { project, records } from '../../../API/requests';
 import { useUserFromStore } from '../../../store/user.slice';
 import Spinner from '../../Spinner';
 import { useForm } from 'react-hook-form';
+import useModal from '../../../hooks/useModal';
+import Modal from '../../Modal';
+import { useHistory } from 'react-router-dom';
+import { getLocaleDate } from '../../../assets/date';
 
 interface IDayRecord {
   newDate: Date;
@@ -17,8 +21,10 @@ interface IData {
 }
 
 function DayRecord({ newDate, selectedProject }: IDayRecord): JSX.Element {
+  const history = useHistory();
   const { user } = useUserFromStore();
   const { register, handleSubmit, watch } = useForm();
+  const { isModal, setIsModal, message, setMessage } = useModal();
 
   const {
     isLoading: projectLoading,
@@ -27,9 +33,17 @@ function DayRecord({ newDate, selectedProject }: IDayRecord): JSX.Element {
   } = useQuery<Project, AxiosError>(['project', selectedProject], () => project.getOne(selectedProject));
 
   const isTimeslot = watch('timeslot');
-  const date = newDate.toISOString();
-
-  const { mutate } = useMutation(records.post);
+  const date = getLocaleDate(newDate).toISOString();
+  // const { mutate, error } = useMutation<unknown, AxiosError>(() => records.delete(id), {
+  //     onSuccess: () => history.push(`/records`),
+  //   });
+  // const { mutate } = useMutation(recordData.put);
+  const { mutate } = useMutation(records.post, {
+    onSuccess: () => {
+      setMessage('Votre rapport à bien été éffectuer');
+      setIsModal((prevState) => !prevState);
+    },
+  });
   const onSubmit = (data: IData) => {
     const { timeslot, comment } = data;
     const record = {
@@ -45,21 +59,35 @@ function DayRecord({ newDate, selectedProject }: IDayRecord): JSX.Element {
     }
     mutate({ ...record, timeslot: timeslot === 'morning' ? 'MORNING' : 'AFTERNOON' });
   };
-  const end = new Date(newDate);
+  const end = new Date(getLocaleDate(newDate));
   end.setDate(end.getDate() + 1);
   const {
     isLoading: recordLoading,
     error: recordError,
     data: recordData,
   } = useQuery<IRecord[], AxiosError>(['records', user.id], () =>
-    project.getRecords(selectedProject, user.id as string, newDate.toISOString(), end.toISOString())
+    project.getRecords(selectedProject, user.id as string, date, end.toISOString())
   );
-  console.log(newDate.getTimezoneOffset());
+  console.log(newDate);
   console.log(recordData);
+
   const error = recordError || projectError;
   if (projectLoading || recordLoading) {
     return <Spinner />;
   }
+  if (isModal)
+    return (
+      <Modal
+        title="Authentification"
+        buttons={
+          data || recordData
+            ? [{ text: 'ok', handleClick: () => history.push('./') }]
+            : [{ text: 'Nouvel essai', handleClick: () => setIsModal((prevState) => !prevState) }]
+        }
+      >
+        {message}
+      </Modal>
+    );
 
   if (error) {
     return (
@@ -72,14 +100,14 @@ function DayRecord({ newDate, selectedProject }: IDayRecord): JSX.Element {
   return (
     <div>
       <div className="flex text-black dark:text-white items-center mx-4 mt-5 sm:mt-20 justify-between">
-        <h1 className="font-bold text-2xl sm:text-4xl">Projet : {data?.name}</h1>
+        <h1 className="font-bold text-sm sm:text-4xl">Projet : {data?.name}</h1>
 
         <h1 className="font-bold sm:text-2xl">{newDate.toLocaleDateString()}</h1>
       </div>
       <form className="sm:mt-20" onSubmit={handleSubmit(onSubmit)}>
         <div className="mt-10 flex flex-col sm:flex-row mx-4">
           <label
-            className={`text-black dark:text-white text-center rounded-xl px-12 py-4   ${
+            className={`text-black dark:text-white text-center rounded-xl px-12 py-4  ${
               isTimeslot === 'morning'
                 ? ' bg-customGreen text-white'
                 : 'border border-black dark:border-white hover:bg-customGreen'
@@ -90,7 +118,7 @@ function DayRecord({ newDate, selectedProject }: IDayRecord): JSX.Element {
           </label>
 
           <label
-            className={`text-black dark:text-white  text-center rounded-xl px-12 py-4 sm:ml-10   ${
+            className={`text-black dark:text-white  text-center rounded-xl px-12 py-4 sm:ml-10 mt-5   ${
               isTimeslot === 'afternoon'
                 ? ' bg-customGreen text-white'
                 : 'border border-black dark:border-white hover:bg-customGreen'
@@ -100,7 +128,7 @@ function DayRecord({ newDate, selectedProject }: IDayRecord): JSX.Element {
             <input className="hidden" type="radio" {...register('timeslot')} id="afternoon" value="afternoon"></input>
           </label>
           <label
-            className={` text-black dark:text-white  text-center rounded-xl px-12 py-4 sm:ml-10   ${
+            className={` text-black dark:text-white  text-center rounded-xl px-12 py-4 sm:ml-10 mt-5   ${
               isTimeslot === 'fullday'
                 ? ' bg-customGreen text-white'
                 : 'border border-black dark:border-white shadow-buttonShadow hover:text-white hover:border-white hover:bg-customGreen'
@@ -113,7 +141,7 @@ function DayRecord({ newDate, selectedProject }: IDayRecord): JSX.Element {
 
         <div className="flex flex-col mt-10 sm:mt-10  mx-4">
           <label htmlFor="text" className="text-white text-xl">
-            Comment
+            Commentaire
           </label>
           <textarea
             {...register('comment')}
