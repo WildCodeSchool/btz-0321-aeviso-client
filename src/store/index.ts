01e8d1dd634e9d0196e0ce7bd983332d3f63b70c
MatthiasWanner <matthias.wanner@gmail.com>
2021-07-11 00:01:10 +0200

    Add reducers at record strore. Configure Redux to accept non serializable values

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,13 +1,18 @@
-import { configureStore } from '@reduxjs/toolkit';
+import { configureStore, getDefaultMiddleware } from '@reduxjs/toolkit';
 
 import statsReducer from './stats.slice';
 import userReducer from './user.slice';
 import darkmodeReducer from './darkmode.slice';
 import recordReducer from './record.slice';
 
+const customizedMiddleware = getDefaultMiddleware({
+  serializableCheck: false,
+});
+
 export const store = configureStore({
   reducer: { user: userReducer, stats: statsReducer, darkMode: darkmodeReducer, record: recordReducer },
   devTools: true,
+  middleware: customizedMiddleware,
 });
 
 // Redux dependencies types
--- src/store/record.slice.ts
+++ src/store/record.slice.ts
@@ -1,10 +1,10 @@
 import { createSlice, PayloadAction } from '@reduxjs/toolkit';
 import { useDispatch, useSelector } from 'react-redux';
 import { RootState } from '.';
+import statsSlice from './stats.slice';
 
 export interface RecordState {
-  date?: Date;
-  companyId?: string;
+  date: Date;
   projetId?: string;
   userId?: string;
   records?: IRecord[];
@@ -12,7 +12,10 @@ export interface RecordState {
 
 interface ReturnUseRecordsFromStore {
   record: RecordState;
-  dispatchCreateRecord: (payload: RecordState) => any; // TODO: find the good type
+  dispatchSelectDate: (payload: Date) => any;
+  dispatchConnectUser: (payload: string) => any;
+  dispatchConnectProject: (payload: string) => any;
+  dispatchCreateRecord: (payload: IRecord[]) => any; // TODO: find the good type
   dispatchDeleteRecord: () => any;
 }
 
@@ -25,21 +28,44 @@ export const recordSlice = createSlice({
   name: 'record',
   initialState,
   reducers: {
-    createRecord: (state, action: PayloadAction<RecordState>) => {
-      return { ...state, ...action.payload };
+    selectDate: (state, action: PayloadAction<Date>) => {
+      return { ...statsSlice, date: action.payload };
+    },
+    connectUser: (state, action: PayloadAction<string>) => {
+      {
+        return { ...state, userId: action.payload };
+      }
+    },
+    connectProject: (state, action: PayloadAction<string>) => {
+      {
+        return { ...state, projetId: action.payload };
+      }
+    },
+    createRecord: (state, action: PayloadAction<IRecord[]>) => {
+      return { ...state, records: action.payload };
     },
     deleteRecord: () => initialState,
   },
 });
 
-export const { createRecord, deleteRecord } = recordSlice.actions;
+export const { createRecord, deleteRecord, selectDate, connectProject, connectUser } = recordSlice.actions;
 
-export const useUserFromStore = (): ReturnUseRecordsFromStore => {
+export const useRecordFromStore = (): ReturnUseRecordsFromStore => {
   const record = useSelector((state: RootState) => state.record);
   const dispatch = useDispatch();
-  const dispatchCreateRecord = (payload: RecordState) => dispatch(createRecord(payload));
+  const dispatchSelectDate = (payload: Date) => dispatch(selectDate(payload));
+  const dispatchConnectUser = (payload: string) => dispatch(connectUser(payload));
+  const dispatchConnectProject = (payload: string) => dispatch(connectProject(payload));
+  const dispatchCreateRecord = (payload: IRecord[]) => dispatch(createRecord(payload));
   const dispatchDeleteRecord = () => dispatch(deleteRecord());
-  return { record, dispatchCreateRecord, dispatchDeleteRecord };
+  return {
+    record,
+    dispatchSelectDate,
+    dispatchConnectUser,
+    dispatchConnectProject,
+    dispatchCreateRecord,
+    dispatchDeleteRecord,
+  };
 };
 
 export default recordSlice.reducer;
