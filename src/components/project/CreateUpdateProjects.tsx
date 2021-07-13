5b23a7c13bc60abf509db1cc7e0079a31f90ddd6
MatthiasWanner <matthias.wanner@gmail.com>
2021-07-13 17:28:11 +0200

    Create components that display a form to create and modify a project in admin dashboard

--- src/components/project/CreateUpdateProjects.tsx
+++ src/components/project/CreateUpdateProjects.tsx
@@ -1,37 +1,22 @@
 import React from 'react';
+import { useParams } from 'react-router-dom';
+import ProjectForm from './ProjectForm';
+import UsersInProject from './UsersInProject';
 
 function CreateUpdateProjects(): JSX.Element {
-  // const {
-  //   isLoading: postLoading,
-  //   error: postError,
-  //   mutate,
-  // } = useMutation(project.create, {
-  //   onSuccess: (data: Project) => {
-  //     setListProject((project) => [...project, data]);
-  //   },
-  // });
+  const { id }: { id: string } = useParams();
 
-  //   const {
-  //     isLoading: updateLoading,
-  //     error: updateError,
-  //     mutate,
-  //   } = useMutation(project.update, { onSuccess: () => history.push('/projects') });
+  if (id)
+    return (
+      <div>
+        <ProjectForm projectId={id} />
+        {id && <UsersInProject projectId={id} />}
+      </div>
+    );
+
   return (
     <div>
-      {/* <div className="w-6/12">
-        <h1 className=" text-2xl font-bold">Post a new project</h1>
-        <form onSubmit={handleSubmit((data) => mutate({ data }))} className="flex flex-col">
-          <label className="mt-5" htmlFor="name">
-            Name
-          </label>
-          <input className="p-2 mt-2 border border-black" {...register('name')} type="text" />
-          <label className="mt-5" htmlFor="description">
-            Description
-          </label>
-          <textarea className="h-28 p-2 mt-2 border border-black" {...register('description')}></textarea>
-          <input className="mt-5 py-1 border border-black" type="submit" />
-        </form>
-      </div> */}
+      <ProjectForm />
     </div>
   );
 }
